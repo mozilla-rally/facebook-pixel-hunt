ed8eaf01fc78ac3fc8023585ec592f0bdaa8dd6a
Robert Helmer <rhelmer@rhelmer.org>
2021-08-10 12:54:04 -0700

    add newline, switch from log to info severity

--- src/background.js
+++ src/background.js
@@ -20,18 +20,18 @@ const rally = new Rally(
   // A sample callback with the study state.
   (newState) => {
     if (newState === runStates.RUNNING) {
-      console.log("pixelHunt collection start");
+      console.info("pixelHunt collection start");
       // Listen for requests to facebook, and then grab the requests to the FB pixel.
       browser.webRequest.onCompleted.addListener(fbPixelListener, { urls: ["*://www.facebook.com/*"] });
     } else {
-      console.log("pixelHunt collection pause");
+      console.info("pixelHunt collection pause");
       browser.webRequest.onCompleted.removeListener(fbPixelListener);
     }
   }
 )
 
 rally.rallyId().then(rallyId =>
-  console.log(`Rally initialized with ID: ${rallyId}`)
+  console.info(`Rally initialized with ID: ${rallyId}`)
 ).catch(err => console.error(err));
 
 // FIXME since this is standalone, there is no way to see from the server if the study is active, so flip it on manually.
